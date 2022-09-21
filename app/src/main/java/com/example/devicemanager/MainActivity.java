2ae15658cbd99d38143ec716d13917644d14446b
Sumaya-1006 <111642545+Sumaya-1006@users.noreply.github.com>
2022-09-21 11:52:48 +0600

    Bottom Sheet

---
+++ app/src/main/java/com/example/devicemanager/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.devicemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.notifyId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+                bottomSheetFragment.show(getSupportFragmentManager(),bottomSheetFragment.getTag());
+
+            }
+        });
+    }
+}
